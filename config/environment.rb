9679ffb88630fc233cb24d95878b8f8fe34df854
Adriana <am20dipi@alum.siena.edu>
2020-12-23 13:03:28 -0500

    commit tree 23c1ff12aea3bd3ea4db4cb973c78d857cbb3174

--- config/environment.rb
+++ config/environment.rb
@@ -14,3 +14,7 @@ SQL
 
 DB[:conn].execute(sql)
 DB[:conn].results_as_hash = true
+
+#Here we are (1) creating the database, (2) dropping table songs to avoid an error, (3) creating the songs table.
+
+#The results_as_hash method is available to use from the SQLite3 - Ruby gem; it ensures that when a select statement is executed, it returns the database row as a hash with the column names as keys. 
